37f323cf2a1b489181f9d4c10d75702a29007e92
zshn1248 <zshn1248@gmail.com>
2025-07-07 18:18:28 +0500

    Updated Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("DOCUMENTATION.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyfilecrypto",
-    version="1.0.3",
+    version="1.0.4",
     author="Muhammad Zeeshan Saeed",
     author_email="zshn1248@gmail.com",
     description="A module for file encryption, decryption, and steganography",
